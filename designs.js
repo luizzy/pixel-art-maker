9c862b78609e6e8553e01d716caf26814af8eb25
luizzy <lewiscrafton95@gmail.com>
2018-07-01 09:23:15 +0300

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,44 +1,31 @@
-const color = document.querySelector('#colorPicker');
-const height = document.querySelector('#inputHeight');
-const width = document.querySelector('#inputWidth');
-const sizePicker = document.querySelector('#sizePicker');
-const canvas = document.querySelector('#pixelCanvas');
 
-function makeGrid() {
-    let tr, td;
+$(function() {
 
-    var rows = $('#inputHeight').val(),
-    cols = $('#inputWidth').val();
-   
-    for (let i = 0; i < row; i++) {
-        tr = document.createElement('tr');
-        canvas.appendChild(tr);
-        for (let j = 0; j < cols; j++) {
-            td = document.createElement('td');
-            tr.appendChild(td);
-        }
-    }
-}
+	function makeGrid() {	 	
+	 	const row = $('#inputHeight').val();			
+	 	const cols = $('#inputWidth').val();		
+		const canvas = $('#pixelCanvas');
+		const enter = $('input[type="submit"]');
+  	canvas.empty();
+		
+		for (let i = 0; i < row; i++) {			
+			canvas.append('<tr></tr>');			
+			for (let j = 0; j < cols; j++) {				
+				canvas.children().last().append('<td></td>');
+				}
+		}
+		
+		$('td').click(function(e) {			
+			let colorPicker = $('#colorPicker').val();			
+			$(e.target).css('background-color', colorPicker);			
+			;
+		});
+	}	
+	enter.click(function(e) {		
+		e.preventDefault();		
+		makeGrid();
+	});
+}); 
 
-function clearGrid() {
-   
-    canvas.innerHTML = '';
-}
 
-function cellColor() {
-   
-    canvas.addEventListener('click', function(e) {
-        e.preventDefault();
-      
-        if (e.target.nodeName === 'TD') {
-            e.target.style.backgroundColor = color.value;
-        }
-    })
-}
 
-sizePicker.addEventListener('submit', function(e) {
-    e.preventDefault();
-    clearGrid();
-    makeGrid();
-    cellColor();
-})
